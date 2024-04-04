0c4e3036080de20990a25a0e39e56f6f779c4b0a
Ekaterina Kim <im.work-k@mail.ru>
2024-04-04 17:07:01 +0300

    test

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,24 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState, useRef } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	//храним состояние(открыта или закрыта панель)
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	
+	// const formElement = useRef<HTMLElement | null>(null);
+
+	const openClose = () => {
+		setIsOpen(prevIsOpen => !prevIsOpen);
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={openClose} isOpen={isOpen}/>
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
